b2e4c4bc1011b9fb44bafbc38dc8fdcdf353baa0
Jedi Ballono <jedidahballono@gmail.com>
2018-10-03 04:12:33 +0800

    route
    Signed-off-by: Jedi Ballono <jedidahballono@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  devise_for :admins
+  resources :articles
+  get "archive", to: "articles#archive"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
