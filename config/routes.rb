37cac931cd5b222bd72deaa6ae039e8c1a80f4aa
Jedidah Ballono <jedidahballono@gmail.com>
2018-10-03 18:07:00 +0800

    Admin dashboard/list view
    Signed-off-by: Kimson Wong <kimsonwong@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :admins
   resources :articles
   get "archive", to: "articles#archive"
+  get "dashboard", to: "admins#dashboard"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
